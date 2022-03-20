f87547b46cb64bb88a0d0629969548bee093f2c7
jokerpoker141 <66033101+jokerpoker141@users.noreply.github.com>
2022-03-20 23:14:32 +0300

    daha sonra coz

--- main.py
+++ main.py
@@ -1,46 +1,80 @@
+import random as r
+import math as m
 import numpy as np
 
-# Taban degerler #
-maxHeight = 1000
-co2 = np.random.randint(400, 500)
-o2 = np.random.randint(400, 500)
-havaOrtalamasi = o2/co2
+class generator:
+    def __init__(self, size, seed, max_value, min_value, water_probability, land_probability):
+        self.size_ = size
+        self.seed_ = r.seed(seed)
+        self.max_value_ = max_value
+        self.min_value_ = min_value
+        self.water_probability_ = water_probability
+        self.land_probability_ = land_probability
+        self.map_ = np.zeros((size, size) ,dtype=int)
+        # mage a vareable to store the median value of the map
+        self.median_ = 0
+    
+    def generate_map(self):
+        # generate the map
+        for i in range(self.size_):
+            for j in range(self.size_):
+                # assing a random number depenfing on the max and minimum values at __init__
+                self.map_[i][j] = r.randint(self.min_value_, self.max_value_)
+                if self.map_[i][j] < self.median_:
+                    self.map_[i][j] = 0
+        # calculate the median value of the map
+        self.median_ = np.median(self.map_)
+        # normalize the map
+        self.normalized_map_ = np.round(self.map_ / self.median_)
+        # return the map
+        return self.map_
+    
+    def smooth_map(self):
+           # smooth the map by averaging the values of the map and the values around it
+        for i in range(self.size_):
+            for j in range(self.size_):
+                if i == 0 or i == self.size_ - 1 or j == 0 or j == self.size_ - 1:
+                    continue
+                else:
+                    if (self.map_[i-1][j] + self.map_[i+1][j] + self.map_[i][j-1] + self.map_[i][j+1]) == 0:
+                        self.map_[i][j] = 0
+                    elif (self.map_[i-1][j] + self.map_[i+1][j] + self.map_[i][j-1] + self.map_[i][j+1]) != 0:
+                        self.map_[i][j] = (self.map_[i-1][j] + self.map_[i+1][j] + self.map_[i][j-1] + self.map_[i][j+1]) / 4
+        # calculate the median value of the map
+        self.median_ = np.median(self.map_)
+        # normalize the map
+        self.normalized_map_ = np.round(self.map_ / self.median_)
+        # return the map
+           
+        return self.map_ , self.median_ , self.normalized_map_
 
-#seed#
-seed = int(input("herhangi bir girdi giriniz"))
-print("UYARI: girdiginiz sayilar kilometre cinsindendir. ")
-genislik = int(input("simulasyonun genisligin giriniz"))
-uzunluk = int(input("simulasyonun uzunlugunu giriiniz"))
-#terrain generation#
-    #Terrain seed bulucu#
-if seed >= 100 and seed < 200:
-    terrainSeed = seed/2 + np.random.randint(50)
-elif seed <= 10:
-    terrainSeed = seed
-    terrainSeed = terrainSeed + np.random.randint(100)
-elif seed > 10 and seed < 100:
-    terrainSeed = seed
-    terrainSeed = terrainSeed + np.random.randint(50)
-elif seed > 200 and seed < 1000:
-    terrainSeed = seed
-    terrainSeed = terrainSeed/10
-    if seed < 50:
-        terrainSeed = terrainSeed + np.random.randint(50)
-else: terrainSeed = print("daha kucuk bir sayi giriniz")
+    def temp_map(self):
+        self.mountain = self.median_ * 0.5
+        self.plato = self.median_ * 0.25
+        self.normal_land = self.median_
+        self.low_land = self.median_ / 2
+        self.temp_map = np.zeros((self.size_, self.size_) ,dtype=str)
+    
+    # generate a temprature map based on the heinght of the map and based on water and land
+    # the higher the value the colder the temprature
+    # the tempratures will affect other tempratures on the map
+    # the temprature map will be used to generate the other maps
+    
+        for i in range(self.size_):
+            for j in range(self.size_):
+                if self.map_[i][j] >= self.mountain:
+                    self.temp_map[i][j] = "mountain"
+                elif self.map_[i][j] >= self.plato:
+                    self.temp_map[i][j] = "plato"
+                elif self.map_[i][j] >= self.normal_land:
+                    self.temp_map[i][j] = "normal_land"
+                elif self.map_[i][j] >= self.low_land:
+                    self.temp_map[i][j] = "low_land"
+                else:
+                    self.temp_map[i][j] = "water"
+            
+        return self.temp_map
+    
 
-terrainSeed = int(terrainSeed)
-    #generation variables
-y = 0
-x = 0 # kordinat sistemi
-
-
-
-with open('test.txt', 'w+') as f:
-    if uzunluk <= 9 and genislik <= 9:
-         for y in range(uzunluk): # uzunluk iterator
-            for x in range(genislik): # genislik iterator
-                f.write(f"[{x+1},{y+1}]")
-            f.write("; \n")# line breaker
-    else: print("daha kucuk bir sayi giriniz")
-
-import terrainGenerator
+generator.__init__(generator,10, r.random(), 100, -100, 0.3, 0.4)
+print(f"{generator.generate_map(generator)} \n \n {generator.smooth_map(generator)} \n \n {generator.temp_map(generator)}")
