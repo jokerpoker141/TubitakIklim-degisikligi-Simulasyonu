5d5e320754003fa6ab783df8efb1db3464fa19a4
jokerpoker141 <66033101+jokerpoker141@users.noreply.github.com>
2022-03-21 22:58:28 +0300

    Categorize eklendi

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import math as m
 import numpy as np
 
 class generator:
-    def __init__(self, size, seed, max_value, min_value, water_probability, land_probability):
+    def __init__(self, size, seed, max_value, min_value, water_probability, temprature):
         self.size_ = size
         self.seed_ = r.seed(seed)
         self.max_value_ = max_value
         self.min_value_ = min_value
         self.water_probability_ = water_probability
-        self.land_probability_ = land_probability
+        self.land_probability_ = 1 - water_probability
         self.map_ = np.zeros((size, size) ,dtype=int)
         # mage a vareable to store the median value of the map
         self.median_ = 0
+        self.temprature_ = temprature
     
     def generate_map(self):
         # generate the map
@@ -25,8 +26,6 @@ class generator:
         # calculate the median value of the map
         self.median_ = np.median(self.map_)
         # normalize the map
-        self.normalized_map_ = np.round(self.map_ / self.median_)
-        # return the map
         return self.map_
     
     def smooth_map(self):
@@ -43,38 +42,81 @@ class generator:
         # calculate the median value of the map
         self.median_ = np.median(self.map_)
         # normalize the map
-        self.normalized_map_ = np.round(self.map_ / self.median_)
-        # return the map
-           
-        return self.map_ , self.median_ , self.normalized_map_
+        normalized_map = self.map_
+        return self.map_ , self.median_ , normalized_map
+    
 
-    def temp_map(self):
-        self.mountain = self.median_ * 0.5
-        self.plato = self.median_ * 0.25
-        self.normal_land = self.median_
-        self.low_land = self.median_ / 2
-        self.temp_map = np.zeros((self.size_, self.size_) ,dtype=str)
-    
-    # generate a temprature map based on the heinght of the map and based on water and land
-    # the higher the value the colder the temprature
-    # the tempratures will affect other tempratures on the map
-    # the temprature map will be used to generate the other maps
-    
+    def categorize_map(self):
+        categorized_map_int = np.zeros((self.size_, self.size_) ,dtype=int)
         for i in range(self.size_):
             for j in range(self.size_):
-                if self.map_[i][j] >= self.mountain:
-                    self.temp_map[i][j] = "mountain"
-                elif self.map_[i][j] >= self.plato:
-                    self.temp_map[i][j] = "plato"
-                elif self.map_[i][j] >= self.normal_land:
-                    self.temp_map[i][j] = "normal_land"
-                elif self.map_[i][j] >= self.low_land:
-                    self.temp_map[i][j] = "low_land"
-                else:
-                    self.temp_map[i][j] = "water"
-            
-        return self.temp_map
+                try:
+                    if self.map_[i][j] > (self.median_*4)/3:
+                        categorized_map_int[i][j] = 1 # 1 = mountain
+                    # if the value is bigger than the median value and has a mountain in a 1 cell radius around it then its a hill
+                    elif self.map_[i][j] > self.median_ and (self.map_[i-1][j] == 1 or self.map_[i+1][j] == 1 or self.map_[i][j-1] == 1 or self.map_[i][j+1] == 1 or self.map_[i-1][j-1] == 1 or self.map_[i+1][j+1] == 1 or self.map_[i-1][j+1] == 1 or self.map_[i+1][j-1] == 1):
+                        categorized_map_int[i][j] = 2 # 2 = hill
+                    # if the value is bigger than the median value and is not a hill then its a plato
+                    elif self.map_[i][j] > self.median_ and (self.map_[i-1][j] != 2 or self.map_[i+1][j] != 2 or self.map_[i][j-1] != 2 or self.map_[i][j+1] != 2 or self.map_[i-1][j-1] != 2 or self.map_[i+1][j+1] != 2 or self.map_[i-1][j+1] != 2 or self.map_[i+1][j-1] != 2):
+                        categorized_map_int[i][j] = 3 # 3 = plain
+                    # if the value is zero or below zero then its water
+                    elif self.map_[i][j] <= 0:
+                        categorized_map_int[i][j] = 4 # 4 = water
+                    # if the value is below the median value then its land and has no water in a 1 cell radius around it
+                    elif self.map_[i][j] <= self.median_ and (self.map_[i-1][j] != 4 or self.map_[i+1][j] != 4 or self.map_[i][j-1] != 4 or self.map_[i][j+1] != 4 or self.map_[i-1][j-1] != 4 or self.map_[i+1][j+1] != 4 or self.map_[i-1][j+1] != 4 or self.map_[i+1][j-1] != 4):
+                        categorized_map_int[i][j] = 5 # 5 = plains
+                    else:
+                        categorized_map_int[i][j] = 0 # 0 = nothing
+                except IndexError:
+                    pass
+                
+            # explatin the categoryized map
+        self.categorized_map_int = categorized_map_int
+        self.categorized_map = np.zeros((self.size_, self.size_), dtype="object")
+        for i in range(self.size_):
+            for j in range(self.size_):
+                if categorized_map_int[i][j] == 1:
+                    self.categorized_map[i][j] = "Mountain"
+                elif categorized_map_int[i][j] == 2:
+                    self.categorized_map[i][j] = "Hill"
+                elif categorized_map_int[i][j] == 3:
+                    self.categorized_map[i][j] = "Plains"
+                elif categorized_map_int[i][j] == 4:
+                    self.categorized_map[i][j] = "Water"
+                elif categorized_map_int[i][j] == 5:
+                    self.categorized_map[i][j] = "Plains"
+        
+        
+        return self.categorized_map, self.categorized_map_int
     
+    def save(self):
+        # saves all the parameters to a file in the same directory as the script file
+        with open("parameters.txt", "w") as f:
+            f.write("size: " + str(self.size_) + "\n")
+            f.write("seed: " + str(self.seed_) + "\n")
+            f.write("max and min height: " + str(self.max_value_) + " " + str(self.min_value_) + "\n")
+            f.write("map: " + str(self.map_) + "\n")
+            f.write("median: " + str(self.median_) + "\n")
+            f.write(f"categorized map :\n  {str(self.categorized_map)} \n")
+            f.write("categorized map int: \n" + str(self.categorized_map_int) + "\n")
+            
 
-generator.__init__(generator,10, r.random(), 100, -100, 0.3, 0.4)
-print(f"{generator.generate_map(generator)} \n \n {generator.smooth_map(generator)} \n \n {generator.temp_map(generator)}")
+        
+                  
+        
+                    
+generator.__init__(generator,10, r.random(), 100000000, -100, 0.3, np.random.randint(10,32))
+generator.generate_map(generator)
+unformatted_map = generator.map_ + 0
+
+#print(unformatted_map)
+
+generator.smooth_map(generator)
+formatted_map = generator.map_
+#print(f"\n \n {formatted_map}" )
+
+generator.categorize_map(generator)
+#print(generator.categorized_map)
+#generator.save(generator)
+#print(f"unformatted_map: {unformatted_map}, \n \n formatted_map: {formatted_map}")
+
